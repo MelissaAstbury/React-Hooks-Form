af0a737d3f731b54a34ab2321957dfa6ed874c66
Melissa Astbury <melissajastbury@gmail.com>
2020-06-08 11:39:14 +0100

    added validation using useForm

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 import "./App.scss";
 
 const App = () => {
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <form>
-      <input type="text" placeholder="Email" name="email" />
-      <input type="password" placeholder="Password" name="password" />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="Email" name="email" ref={register} />
+      <input
+        type="password"
+        placeholder="Password"
+        name="password"
+        ref={register({
+          required: "PASSWORD REQUIRED",
+          minLength: { value: 8, message: "TOO SHORT" },
+        })}
+      />
+      {errors.password && <p>{errors.password.message}</p>}
       <input type="submit" />
+      {/* disabled={errors.password && "disabled"} */}
     </form>
   );
 };
